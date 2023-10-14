f4595defa06e82add76f50261c45fdc608d18d5d
Vitaliy Shatalov <vvshatalov@avito.ru>
2023-10-14 21:06:16 +0700

    map fix

--- apps/client/src/pages/map/MapPage.tsx
+++ apps/client/src/pages/map/MapPage.tsx
@@ -1,14 +1,10 @@
 import Map from '@/widgets/map'
-import { useRef } from 'react'
-import { Map as LeafletMap } from 'leaflet'
 
 export const MapPage = () => {
-  const ref = useRef<LeafletMap>(null)
-
   return (
     <div className='grow flex flex-col'>
       <div className='flex grow [&>*]:grow flex-col'>
-        <Map ref={ref} />
+        <Map />
       </div>
       {/*<div className='absolute w-full h-full pointer-events-none'>*/}
       {/*  <p>Some text</p>*/}
--- apps/client/src/widgets/map/Map.tsx
+++ apps/client/src/widgets/map/Map.tsx
@@ -1,4 +1,4 @@
-import { FC, forwardRef, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import { Map as LeafletMap } from 'leaflet'
 import { Position } from '@/pages/map/types.ts'
@@ -7,7 +7,7 @@ import axios from 'axios'
 import { GeoIP } from '@/widgets/map/types.ts'
 import { Loader } from '@/components/Loader.tsx'
 
-export const Map = forwardRef<LeafletMap, {}>(props => {
+export const Map = () => {
   const ref = useRef<LeafletMap>(null)
   const container = useRef<HTMLDivElement>(null)
   const [currentPos, setCurrentPos] = useState<Position | undefined>()
@@ -18,6 +18,7 @@ export const Map = forwardRef<LeafletMap, {}>(props => {
       .handlePermission()
       .then(data => {
         setAllowed(true)
+        console.log(data.coords)
         setCurrentPos({
           latitude: data.coords.latitude,
           longitude: data.coords.longitude,
@@ -25,26 +26,44 @@ export const Map = forwardRef<LeafletMap, {}>(props => {
       })
       .catch(() => {
         axios.get<GeoIP>('http://ip-api.com/json/').then(res => {
-          // setCurrentPos({ latitude: res.data.lat, longitude: res.data.lon })
+          setCurrentPos({ latitude: res.data.lat, longitude: res.data.lon })
         })
       })
   }, [])
 
+  useEffect(() => {
+    console.log('call', currentPos, ref.current)
+    if (currentPos && ref.current) {
+      console.log('chjeck')
+      ref.current?.setView(
+        {
+          lat: currentPos.latitude,
+          lng: currentPos.longitude,
+        },
+        allowed ? 13 : 11,
+      )
+    }
+  }, [currentPos, ref, allowed])
+
   return (
     <div className='flex grow' ref={container}>
-      {currentPos ? (
-        <MapContainer ref={ref} className='grow' scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            // url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            url='https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
-          />
-        </MapContainer>
-      ) : (
+      <MapContainer
+        ref={ref}
+        className='grow'
+        center={[0, 0]}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          // url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          url='https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
+      {!currentPos && (
         <div className='h-full w-full flex flex-col items-center justify-center'>
           <Loader className='h-8 w-8' />
         </div>
       )}
     </div>
   )
-})
+}
