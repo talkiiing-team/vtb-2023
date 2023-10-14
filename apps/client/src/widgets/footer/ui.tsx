7b8b3789bbcdacabccf1937e02e37c64ce4df0c2
desolaye <desolaye@icloud.com>
2023-10-14 17:01:51 +0700

    chore(client): footer update

--- apps/client/src/widgets/footer/ui.tsx
+++ apps/client/src/widgets/footer/ui.tsx
@@ -1,13 +1,37 @@
-import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import { Link, useLocation } from 'react-router-dom'
+
+const linksNav = [
+  {
+    icon: 'B',
+    path: '/bank-list',
+  },
+  {
+    icon: 'H',
+    path: '/',
+  },
+  {
+    icon: 'M',
+    path: '/map',
+  },
+]
 
 export const Footer = () => {
+  const { pathname } = useLocation()
+
   return (
     <footer className="btm-nav bg-primary text-white rounded-t-lg">
-      <Link to={'/banks'}>B</Link>
-      <Link to={'/'} className="bg-secondary">
-        H
-      </Link>
-      <Link to={'/map'}>M</Link>
+      {linksNav.map((e, i) => (
+        <Link
+          key={i}
+          to={e.path}
+          className={classNames('transition-all rounded-t-lg', {
+            ['bg-secondary']: e.path === pathname,
+          })}
+        >
+          {e.icon}
+        </Link>
+      ))}
     </footer>
   )
 }
