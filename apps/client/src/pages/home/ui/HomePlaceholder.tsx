4575efe8bb8e375b63bfa1bbd000bb5f9870bf74
desolaye <desolaye@icloud.com>
2023-10-14 21:40:35 +0700

    feat(client): final push

--- apps/client/src/pages/home/ui/HomePlaceholder.tsx
+++ apps/client/src/pages/home/ui/HomePlaceholder.tsx
@@ -3,33 +3,33 @@ import LogoVTB from '@/shared/assets/icons/logo-vtb.png'
 
 export const HomePlaceholder = () => {
   return (
-    <article className="grow flex flex-col justify-between opacity-50">
+    <article className='grow flex flex-col justify-between opacity-50'>
       <section>
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <div className="w-8 h-8 rotate-180">
+        <div className='flex flex-col gap-4 justify-center items-center'>
+          <div className='w-8 h-8 rotate-180'>
             <ArrowIcon />
           </div>
-          <p className="text-center">Ваш запрос</p>
+          <p className='text-center'>Ваш запрос</p>
         </div>
       </section>
-      <section>
-        <img src={LogoVTB} alt="logo vtb" />
+      <section className='mx-auto'>
+        <img src={LogoVTB} alt='logo vtb' />
       </section>
-      <section className="flex justify-between">
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <p className="text-center">
+      <section className='flex justify-between'>
+        <div className='flex flex-col gap-4 justify-center items-center'>
+          <p className='text-center'>
             Ближайшие
             <br /> банкоматы
           </p>
-          <div className="w-8 h-8">
+          <div className='w-8 h-8'>
             <ArrowIcon />
           </div>
         </div>
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <p className="text-center">
+        <div className='flex flex-col gap-4 justify-center items-center'>
+          <p className='text-center'>
             Карта <br /> банков
           </p>
-          <div className="w-8 h-8">
+          <div className='w-8 h-8'>
             <ArrowIcon />
           </div>
         </div>
--- apps/client/src/widgets/layouts/root-layout/ui.tsx
+++ apps/client/src/widgets/layouts/root-layout/ui.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react'
 import Footer from '@/widgets/footer'
+import { Header } from '@/widgets/header'
 
 export const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <article className="flex flex-col min-h-screen pb-16">
-      <header>aboba</header>
-      <main className="grow flex flex-col">{children}</main>
+    <article className='flex flex-col min-h-screen pb-16'>
+      <Header />
+      <main className='grow flex flex-col'>{children}</main>
       <Footer />
     </article>
   )
