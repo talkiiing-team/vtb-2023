afa2fb1ec954cfd9d399740fccd130e75efe4de2
Nerlih Max <nerlihmax@yandex-team.ru>
2023-10-15 13:37:51 +0700

    fwef

--- apps/server/src/api/v1/banks/router.ts
+++ apps/server/src/api/v1/banks/router.ts
@@ -3,7 +3,7 @@ import { Router } from 'express'
 import controller from './controller'
 
 const router = Router()
-  .get('/:uuid', controller.getById)
-  .get('/:uuid/load', controller.getLoadById)
   .get('/nearest', controller.getNearest)
+  .get('/:uuid/load', controller.getLoadById)
+  .get('/:uuid', controller.getById)
 export default router
