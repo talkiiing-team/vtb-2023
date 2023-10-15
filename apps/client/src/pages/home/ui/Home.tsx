30475a931048dd817b93d29e3d78d20b82b154c6
desolaye <desolaye@icloud.com>
2023-10-15 12:56:00 +0700

    chore(client): home placeholder

--- apps/client/src/pages/home/ui/Home.tsx
+++ apps/client/src/pages/home/ui/Home.tsx
@@ -1,6 +1,7 @@
 import { Search } from '@/features/Search'
 import { HomePlaceholder } from './HomePlaceholder'
 import { useState } from 'react'
+import { FaceFrownIcon } from '@heroicons/react/24/outline'
 
 export const Home = () => {
   const [search, setSearch] = useState('')
@@ -8,10 +9,13 @@ export const Home = () => {
   const onSearch = (line: string) => setSearch(line)
 
   return (
-    <article className="grow flex flex-col p-4 gap-2">
+    <article className='grow flex flex-col p-4 gap-2'>
       <Search onSearch={onSearch} />
       {search.length > 1 ? (
-        <main className="grow flex-1">content</main>
+        <main className='grow flex-1 text-center flex flex-col justify-center items-center gap-4'>
+          <FaceFrownIcon width={48} height={48} />
+          <p>Ууупс. Кажется здесь должна быть обработка запроса...?</p>
+        </main>
       ) : (
         <HomePlaceholder />
       )}
